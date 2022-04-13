cf8fa153e0d19c403d872fdd70ebb81fb0f7c4a5
Lena <olena.ratushna21@gmail.com>
2022-04-13 11:21:43 +0300

    Finished slider

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,19 @@
 const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
-    spaceBetween: 30,
+    spaceBetween: 20,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
     breakpoints: { 
+        480: {
+            slidesPerView: 2,
+            paceBetween: 25,
+        },
+        767: {
+            slidesPerView: 3,
+            paceBetween: 30,
+        },
         992: {
             slidesPerView: 3,
             spaceBetween: 40,
